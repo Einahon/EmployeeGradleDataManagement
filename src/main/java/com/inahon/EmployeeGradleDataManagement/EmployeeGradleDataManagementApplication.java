57bd343e3122a098f6f831eeeeccf0d054436b1d
elie <inahon.elie17@yahoo.com>
2025-01-02 02:20:33 -0500

    Initial commit

---
+++ src/main/java/com/inahon/EmployeeGradleDataManagement/EmployeeGradleDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.inahon.EmployeeGradleDataManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeGradleDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeGradleDataManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inahon/EmployeeGradleDataManagement/EmployeeGradleDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inahon.EmployeeGradleDataManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeGradleDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
