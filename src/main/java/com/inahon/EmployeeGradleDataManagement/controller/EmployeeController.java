fdd648b12c0b6155dbc45502cc9281b97529b2c3
elie <inahon.elie17@yahoo.com>
2025-01-02 20:57:12 -0500

    Created save methods in the controller class, mapped the endpoint and added necessary annotations.

--- src/main/java/com/inahon/EmployeeGradleDataManagement/controller/EmployeeController.java
+++ src/main/java/com/inahon/EmployeeGradleDataManagement/controller/EmployeeController.java
@@ -1,10 +1,20 @@
 package com.inahon.EmployeeGradleDataManagement.controller;
 
 import com.inahon.EmployeeGradleDataManagement.model.Employee;
+import com.inahon.EmployeeGradleDataManagement.service.Impl.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-
+    @Autowired
+    private EmployeeService employeeService;
+    @PostMapping("/employees")
+    public Employee saveEmployee(@Valid @RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
 
 }
