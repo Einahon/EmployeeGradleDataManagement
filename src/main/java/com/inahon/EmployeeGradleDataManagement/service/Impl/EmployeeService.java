fbd4b2d55ef17c62245e654ad0f6d6ba3715cc27
elie <inahon.elie17@yahoo.com>
2025-01-02 21:04:20 -0500

    Override  save method in the service class since it implements Interface service

--- src/main/java/com/inahon/EmployeeGradleDataManagement/service/Impl/EmployeeService.java
+++ src/main/java/com/inahon/EmployeeGradleDataManagement/service/Impl/EmployeeService.java
@@ -1,6 +1,17 @@
 package com.inahon.EmployeeGradleDataManagement.service.Impl;
 
+import com.inahon.EmployeeGradleDataManagement.model.Employee;
+import com.inahon.EmployeeGradleDataManagement.repository.EmployeeRepository;
 import com.inahon.EmployeeGradleDataManagement.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeService implements IEmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
