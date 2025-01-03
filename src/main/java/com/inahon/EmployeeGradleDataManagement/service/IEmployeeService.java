66a1d98f0531402179b0d60e9a6e368c15bf54bc
elie <inahon.elie17@yahoo.com>
2025-01-02 21:00:52 -0500

    Created save method in the Interface service

--- src/main/java/com/inahon/EmployeeGradleDataManagement/service/IEmployeeService.java
+++ src/main/java/com/inahon/EmployeeGradleDataManagement/service/IEmployeeService.java
@@ -1,4 +1,8 @@
 package com.inahon.EmployeeGradleDataManagement.service;
 
+import com.inahon.EmployeeGradleDataManagement.model.Employee;
+import com.inahon.EmployeeGradleDataManagement.repository.EmployeeRepository;
+
 public interface IEmployeeService {
+   public Employee saveEmployee(Employee employee);
 }
